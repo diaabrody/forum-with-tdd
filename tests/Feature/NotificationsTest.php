a7cc46a9184d9a65e6a65c84364773f3320153cc
diaa <diaa_osama70@outlook.com>
2020-08-16 00:11:30 +0200

    use Notifications fakes in test

--- tests/Feature/NotificationsTest.php
+++ tests/Feature/NotificationsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Activity;
+use App\Notifications\ThreadUpdated;
 use App\Thread;
 use App\User;
 use Illuminate\Auth\AuthenticationException;
@@ -10,6 +11,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Support\Facades\Notification;
 use Tests\CustomTestCase;
 use Tests\TestCase;
 
@@ -21,14 +23,15 @@ class NotificationsTest extends CustomTestCase
     }
 
     public function test_a_notification_is_prepared_when_a_subscribed_thread_receives_a_new_reply_that_is_not_by_the_current_user(){
+        Notification::fake();
         $thread = create('App\Thread');
         $thread->subscribe();
-        $this->assertCount(0 , auth()->user()->notifications);
 
         $thread->addReplay([
             'user_id'=>auth()->user()->id,
             'body'=>'hi'
         ]);
+        Notification::assertNothingSent();
 
         $this->assertCount(0 , auth()->user()->fresh()->notifications);
 
@@ -38,7 +41,7 @@ class NotificationsTest extends CustomTestCase
             'body'=>'hi'
         ]);
 
-        $this->assertCount(1 , auth()->user()->fresh()->notifications);
+        Notification::assertSentTo(auth()->user() , ThreadUpdated::class);
     }
 
 
